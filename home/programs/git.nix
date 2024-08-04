36a086d8e2c0dc2c078a2853ea4af0e504bb59d8
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-08-03 17:21:31 -0700

    fix ssh and git

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -2,6 +2,8 @@
 {
   programs.git = {
     enable = true;
+    userName = "Jason Elie Bou Kheir";
+    userEmail = "5115126+jasonboukheir@users.noreply.github.com";
     extraConfig = {
       init.defaultBranch = "main";
     };
