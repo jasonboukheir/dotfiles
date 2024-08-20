392af3a28414689695c647b9c5399a631fd8c87e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-08-19 20:18:52 -0700

    fix finder default style

--- modules/darwin/system/finder.nix
+++ modules/darwin/system/finder.nix
@@ -2,18 +2,19 @@
 {
   system.defaults.finder = {
     _FXShowPosixPathInTitle = true;
+    AppleShowAllFiles = true;
     AppleShowAllExtensions = true;
     CreateDesktop = false;
     FXEnableExtensionChangeWarning = false;
     QuitMenuItem = true;
     ShowPathbar = true;
     ShowStatusBar = true;
+    FXDefaultSearchScope = "SCcf";
+    FXPreferredViewStyle = "Nlsv";
   };
   system.defaults.CustomUserPreferences = {
     "com.apple.finder" = {
       _FXSortFoldersFirst = true;
-      # When performing a search, search the current folder by default
-      FXDefaultSearchScope = "SCcf";
     };
   };
 }
