a6ad45118b07c0fe20c4376ea6ea8abdb29cfda9
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-08-21 20:30:28 -0700

    apply settings immediately instead of logout login

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -1,14 +1,11 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     users.jasonbk.home = {
       stateVersion = "24.05";
-      packages = with pkgs; [
-        neovim
-        (nerdfonts.override { fonts = [ "FiraCode" ]; })
-      ];
+      packages = with pkgs; [ (nerdfonts.override { fonts = [ "FiraCode" ]; }) ];
     };
   };
 }
