b7a533b474d8dada96401e3c60cf2156107e7db7
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-09-25 11:29:02 -0500

    add ipfs

--- hosts/m1/system/dock.nix
+++ hosts/m1/system/dock.nix
@@ -1,13 +1,10 @@
-{ inputs, pkgs, ... }:
-let
-  pkgs-zed-fix = inputs.nixpkgs-zed-fix.legacyPackages.${pkgs.system};
-in
+{ pkgs, ... }:
 {
   system.defaults.dock = {
     persistent-apps = [
       "/System/Cryptexes/App/System/Applications/Safari.app"
       "${pkgs.kitty}/Applications/kitty.app"
-      "${pkgs-zed-fix.zed-editor}/Applications/Zed.app"
+      "/Applications/Zed.app"
       "/System/Applications/Messages.app"
       "/System/Applications/Mail.app"
       "/System/Applications/Music.app"
