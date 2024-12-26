a0676001fb341c76752e5a22e105bc110897cd8a
Jason Bou Kheir <jasonbk@meta.com>
2024-12-26 13:35:13 -0800

    update persistent-apps with Ghostty

--- hosts/m1/system/dock.nix
+++ hosts/m1/system/dock.nix
@@ -3,7 +3,7 @@
   system.defaults.dock = {
     persistent-apps = [
       "/System/Cryptexes/App/System/Applications/Safari.app"
-      "${pkgs.kitty}/Applications/kitty.app"
+      "System/Applications/Ghostty.app"
       "${pkgs.zed-editor}/Applications/Zed.app"
       "/System/Applications/Messages.app"
       "/System/Applications/Mail.app"
