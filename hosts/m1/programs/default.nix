4248a09f8c16a786c191b09342a8aa1d3a749428
Jason Bou Kheir <jasonbk@meta.com>
2024-12-30 09:27:05 -0800

    make zed available to all darwin systems

--- hosts/m1/programs/default.nix
+++ hosts/m1/programs/default.nix
@@ -4,7 +4,6 @@
     ./brave.nix
     ./discord.nix
     ./dotnet.nix
-    ./zed.nix
     ./vscode.nix
     ./proton-drive.nix
     ./iina.nix
---
+++ hosts/m3/system/default.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  imports = [
+    ./dock.nix
+    ./ssh.nix
+  ];
+}
