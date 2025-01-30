97b404114e17629902049aff0b3d6b5924c36ed7
Jason Bou Kheir <jasonbk@meta.com>
2025-01-30 09:28:38 -0800

    move nix related settings into a file to better remember where it is

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,20 +1,9 @@
-{ inputs, ... }:
+{ ... }:
 {
   imports = [
     ./home-manager.nix
     ./fonts.nix
+    ./nix-settings.nix
     ./users.nix
   ];
-
-  services.nix-daemon.enable = true;
-  nix.settings = {
-    access-tokens = "!include ./.secrets/github.pat";
-    experimental-features = "nix-command flakes";
-  };
-  nixpkgs.config.allowUnfree = true;
-
-  system = {
-    stateVersion = 4;
-    configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-  };
 }
---
+++ modules/system/nix-settings.nix
@@ -0,0 +1,14 @@
+{ inputs, ... }:
+{
+  services.nix-daemon.enable = true;
+  nix.settings = {
+    access-tokens = "!include ./.secrets/github.pat";
+    experimental-features = "nix-command flakes";
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  system = {
+    stateVersion = 4;
+    configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+  };
+}
