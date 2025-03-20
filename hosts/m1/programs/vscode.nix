44030a33d21845b94bec7a3ef37c47be1b6d4671
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-03-19 20:40:27 -0700

    upgrade flakes, turn off vscode and discord

--- hosts/m1/programs/vscode.nix
+++ hosts/m1/programs/vscode.nix
@@ -9,130 +9,127 @@ in
     programs.vscode = {
       enable = true;
       package = pkgs.vscode;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
       mutableExtensionsDir = false;
-      extensions =
-        with pkgs.vscode-extensions;
-        [
-          jnoortheen.nix-ide
-          arcticicestudio.nord-visual-studio-code
-          huytd.nord-light
-          pkief.material-icon-theme
-          ms-dotnettools.vscode-dotnet-runtime
-          ms-dotnettools.csharp
-          ms-dotnettools.csdevkit
-          ms-python.python
-        ]
-        ++ (with marketplace-extensions; [
-          marlosirapuan.nord-deep
-          visualstudiotoolsforunity.vstuc
-        ]);
+      profiles.default = {
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
+        extensions =
+          with pkgs.vscode-extensions;
+          [
+            jnoortheen.nix-ide
+            arcticicestudio.nord-visual-studio-code
+            huytd.nord-light
+            pkief.material-icon-theme
+          ]
+          ++ (with marketplace-extensions; [
+            marlosirapuan.nord-deep
+          ]);
 
-      userSettings = {
-        editor = {
-          acceptSuggestionOnCommitCharacter = false;
-          acceptSuggestionOnEnter = "off";
-          accessibilitySupport = "off";
-          codeLens = true;
-          colorDecorators = true;
-          cursorBlinking = "phase";
-          cursorStyle = "underline";
-          emptySelectionClipboard = false;
-          fontFamily = "'FiraCode Nerd Font'";
-          fontLigatures = true;
-          fontSize = 12;
-          fontWeight = "500";
-          formatOnPaste = true;
-          formatOnSave = true;
-          lightbulb.enabled = "off";
-          minimap.enabled = false;
-          multiCursorModifier = "alt";
-          renderWhitespace = "selection";
-          smoothScrolling = true;
-          snippetSuggestions = "top";
-          tabCompletion = "onlySnippets";
-          tabSize = 4;
-          tokenColorCustomizations."[Nord]".textMateRules = [ ];
-          wordWrapColumn = 120;
-        };
-        explorer = {
-          autoReveal = true;
-          incrementalNaming = "smart";
-          openEditors.visible = 0;
-        };
-        extensions = {
-          closeExtensionDetailsOnViewChange = true;
-        };
-        files = {
-          eol = "\n";
-          insertFinalNewline = true;
-        };
-        problems = {
-          autoReveal = false;
-        };
-        security.workspace.trust.untrustedFiles = "open";
-        search = {
-          collapseResults = "alwaysCollapse";
-          useGlobalIgnoreFiles = true;
-        };
-        telemetry.telemetryLevel = "off";
-        terminal = {
-          explorerKind = "external";
-          external = {
-            linuxExec = "tilix";
-            osxExex = "kitty";
-          };
-          integrated = {
-            cursorStyle = "underline";
-            fontSize = 12;
-          };
-        };
-        window = {
-          autoDetectColorScheme = true;
-          autoDetectHighContrast = true;
-          enableMenuBarMnemonics = false;
-          menuBarVisibility = "hidden";
-          titleBarStyle = "native";
-        };
-        workbench = {
-          activityBar.visible = true;
-          colorTheme = "Nord";
-          preferredDarkColorTheme = "Nord";
-          preferredLightColorTheme = "Nord Light";
-          preferredHighContrastColorTheme = "Nord Midnight";
-          preferredHighContrastLightColorTheme = "Nord Light Brighter";
-          commandPalette.preserveInput = true;
+        userSettings = {
           editor = {
-            enablePreview = false;
-            enablePreviewFromQuickOpen = false;
-            focusRecentEditorAfterClose = false;
+            acceptSuggestionOnCommitCharacter = false;
+            acceptSuggestionOnEnter = "off";
+            accessibilitySupport = "off";
+            codeLens = true;
+            colorDecorators = true;
+            cursorBlinking = "phase";
+            cursorStyle = "underline";
+            emptySelectionClipboard = false;
+            fontFamily = "'FiraCode Nerd Font'";
+            fontLigatures = true;
+            fontSize = 12;
+            fontWeight = "500";
+            formatOnPaste = true;
+            formatOnSave = true;
+            lightbulb.enabled = "off";
+            minimap.enabled = false;
+            multiCursorModifier = "alt";
+            renderWhitespace = "selection";
+            smoothScrolling = true;
+            snippetSuggestions = "top";
+            tabCompletion = "onlySnippets";
+            tabSize = 4;
+            tokenColorCustomizations."[Nord]".textMateRules = [ ];
+            wordWrapColumn = 120;
           };
-          enableExperiments = false;
-          iconTheme = "material-icon-theme";
-          quickOpen.preserveInput = true;
-          settings.enableNaturalLanguageSearch = false;
-          sideBar.location = "left";
-          startupEditor = "welcomePage";
-          statusBar.feedback.visible = false;
-          renderIndentGuides = "none";
-        };
-
-        # Extensions
-        "material-icon-theme" = {
-          activeIconPack = "react";
-          folders = {
-            color = "#616e88";
-            theme = "classic";
+          explorer = {
+            autoReveal = true;
+            incrementalNaming = "smart";
+            openEditors.visible = 0;
           };
-          hideExplorerArrows = true;
-          saturation = 0.6;
-        };
+          extensions = {
+            closeExtensionDetailsOnViewChange = true;
+          };
+          files = {
+            eol = "\n";
+            insertFinalNewline = true;
+          };
+          problems = {
+            autoReveal = false;
+          };
+          security.workspace.trust.untrustedFiles = "open";
+          search = {
+            collapseResults = "alwaysCollapse";
+            useGlobalIgnoreFiles = true;
+          };
+          telemetry.telemetryLevel = "off";
+          terminal = {
+            explorerKind = "external";
+            external = {
+              linuxExec = "tilix";
+              osxExex = "kitty";
+            };
+            integrated = {
+              cursorStyle = "underline";
+              fontSize = 12;
+            };
+          };
+          window = {
+            autoDetectColorScheme = true;
+            autoDetectHighContrast = true;
+            enableMenuBarMnemonics = false;
+            menuBarVisibility = "hidden";
+            titleBarStyle = "native";
+          };
+          workbench = {
+            activityBar.visible = true;
+            colorTheme = "Nord";
+            preferredDarkColorTheme = "Nord";
+            preferredLightColorTheme = "Nord Light";
+            preferredHighContrastColorTheme = "Nord Midnight";
+            preferredHighContrastLightColorTheme = "Nord Light Brighter";
+            commandPalette.preserveInput = true;
+            editor = {
+              enablePreview = false;
+              enablePreviewFromQuickOpen = false;
+              focusRecentEditorAfterClose = false;
+            };
+            enableExperiments = false;
+            iconTheme = "material-icon-theme";
+            quickOpen.preserveInput = true;
+            settings.enableNaturalLanguageSearch = false;
+            sideBar.location = "left";
+            startupEditor = "welcomePage";
+            statusBar.feedback.visible = false;
+            renderIndentGuides = "none";
+          };
 
-        nix = {
-          formatterPath = "nixfmt";
+          # Extensions
+          "material-icon-theme" = {
+            activeIconPack = "react";
+            folders = {
+              color = "#616e88";
+              theme = "classic";
+            };
+            hideExplorerArrows = true;
+            saturation = 0.6;
+          };
+
+          nix = {
+            formatterPath = "nixfmt";
+          };
+          "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${pkgs.dotnet-sdk_8}";
         };
-        "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${pkgs.dotnet-sdk_8}";
       };
     };
   };
