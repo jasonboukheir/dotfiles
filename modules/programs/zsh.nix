0888deeab8a59c85813da397236503ea2d246261
Jason Bou Kheir <jasonbk@meta.com>
2025-07-29 08:40:29 -0700

    add perf test stuff

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.zsh.enable = true;
   home-manager.users.jasonbk = {
@@ -11,6 +11,16 @@
       shellAliases = {
         git = "${pkgs.git}/bin/git";
       };
+      # perf test zsh startup
+      # initContent =
+      #   let
+      #     zshConfigStart = lib.mkOrder 100 "zmodload zsh/zprof";
+      #     zshConfigEnd = lib.mkOrder 2000 "zprof";
+      #   in
+      #   lib.mkMerge [
+      #     zshConfigStart
+      #     zshConfigEnd
+      #   ];
     };
   };
 }
