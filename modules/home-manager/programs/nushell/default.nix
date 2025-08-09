3017390277a0d9c78c3c2649535cc4f37dc6938e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-08 23:53:25 -0700

    move the extraConfig and extraEnv to darwin

--- modules/home-manager/programs/nushell/default.nix
+++ modules/home-manager/programs/nushell/default.nix
@@ -3,12 +3,6 @@
   config = lib.mkIf config.programs.nushell.enable {
     programs.nushell = {
       configFile.source = ./config.nu;
-      extraEnv = ''
-        source /etc/nushell/system-env.nu
-      '';
-      extraConfig = ''
-        source /etc/nushell/system-config.nu
-      '';
     };
     home.shell.enableNushellIntegration = true;
   };
