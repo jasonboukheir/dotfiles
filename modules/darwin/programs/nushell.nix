77f806b4d52d9bc2fc8958fcee701ff080fbcd7d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-09 10:42:31 -0700

    fix nushell issue on nixos, todo: refactor it to just be a home-manager module

--- modules/darwin/programs/nushell.nix
+++ modules/darwin/programs/nushell.nix
@@ -12,7 +12,7 @@ in
   options = {
     programs.nushell.enable = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       description = "Whether to configure Nushell as an interactive shell.";
     };
     programs.nushell.variables = mkOption {
--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -2,6 +2,5 @@
 {
   imports = [
     ./git.nix
-    ./nushell.nix
   ];
 }
