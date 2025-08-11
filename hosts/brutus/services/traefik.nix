1ee475584d8c3f255c42a271a4c7ccd06602c539
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-10 21:21:31 -0700

    it's working!

--- hosts/brutus/services/traefik.nix
+++ hosts/brutus/services/traefik.nix
@@ -1,53 +1,51 @@
 { config, ... }:
 {
   services.traefik = {
-      enable = true;
-
-      environmentFiles = [
-        "/var/lib/secrets/sunnycareboo.com.cloudflare.token"
-      ];
-
-      staticConfigOptions = {
-        entryPoints = {
-          web = {
-            address = ":80";
-            asDefault = true;
-            http.redirections.entrypoint = {
-              to = "websecure";
-              scheme = "https";
-            };
-          };
-
-          websecure = {
-            address = ":443";
-            asDefault = true;
-            http.tls.certResolver = "letsencrypt";
+    enable = true;
+    environmentFiles = [
+      "/var/lib/secrets/sunnycareboo.com.cloudflare.token"
+    ];
+    staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+          asDefault = true;
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
           };
         };
-
-        log = {
-          level = "INFO";
-          filePath = "${config.services.traefik.dataDir}/traefik.log";
-          format = "json";
+        websecure = {
+          address = ":443";
+          asDefault = true;
+          http.tls.certResolver = "letsencrypt";
         };
-
-        certificatesResolvers.letsencrypt.acme = {
-          email = "postmaster@sunnycareboo.com";
-          storage = "${config.services.traefik.dataDir}/acme.json";
-          dnsChallenge = {
-            provider = "cloudflare";
-            resolvers = ["8.8.8.8:53" "8.8.4.4:53"];
-          };
-        };
-
-        api.dashboard = true;
-        # Access the Traefik dashboard on <Traefik IP>:8080 of your server
-        # api.insecure = true;
       };
-
-      dynamicConfigOptions = {
-        http.routers = {};
-        http.services = {};
+      log = {
+        level = "INFO";
+        filePath = "${config.services.traefik.dataDir}/traefik.log";
+        format = "json";
+      };
+      certificatesResolvers.letsencrypt.acme = {
+        email = "postmaster@sunnycareboo.com";
+        storage = "${config.services.traefik.dataDir}/acme.json";
+        dnsChallenge = {
+          provider = "cloudflare";
+          resolvers = ["8.8.8.8:53" "8.8.4.4:53"];
+        };
+      };
+      api.dashboard = true;
+    };
+    dynamicConfigOptions = {
+      http = {
+        routers = {
+          dashboard = {
+            rule = ''Host(`traefik.sunnycareboo.com`) && (PathPrefix(`/api`) || PathPrefix(`/dashboard`))'';
+            service = "api@internal";
+            entryPoints = [ "websecure" ];
+          };
+        };
       };
     };
+  };
 }
