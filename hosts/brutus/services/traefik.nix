61bda0bbbb35e4318746c2a0c5a5475d58911254
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-10 20:27:04 -0700

    add traefik service

---
+++ hosts/brutus/services/traefik.nix
@@ -0,0 +1,53 @@
+{ ... }:
+{
+  services.traefik = {
+      enable = true;
+
+      environmentFiles = [
+        "/var/lib/secrets/sunnycareboo.com.cloudflare.token",
+      ];
+
+      staticConfigOptions = {
+        entryPoints = {
+          web = {
+            address = ":80";
+            asDefault = true;
+            http.redirections.entrypoint = {
+              to = "websecure";
+              scheme = "https";
+            };
+          };
+
+          websecure = {
+            address = ":443";
+            asDefault = true;
+            http.tls.certResolver = "letsencrypt";
+          };
+        };
+
+        log = {
+          level = "INFO";
+          filePath = "${config.services.traefik.dataDir}/traefik.log";
+          format = "json";
+        };
+
+        certificatesResolvers.letsencrypt.acme = {
+          email = "postmaster@sunnycareboo.com";
+          storage = "${config.services.traefik.dataDir}/acme.json";
+          dnsChallenge = {
+            provider = "cloudflare";
+            resolvers = ["8.8.8.8:53", "8.8.4.4:53"];
+          };
+        };
+
+        api.dashboard = true;
+        # Access the Traefik dashboard on <Traefik IP>:8080 of your server
+        # api.insecure = true;
+      };
+
+      dynamicConfigOptions = {
+        http.routers = {};
+        http.services = {};
+      };
+    };
+}
