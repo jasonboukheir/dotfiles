30fffa827df2aa363b36fcb34c1977afe169c01e
Jason Bou Kheir <jasonbk@meta.com>
2025-08-12 11:08:53 -0700

    add agentpath and sign path to 1password options

--- modules/home-manager/sharedModules/programs/_1password.nix
+++ modules/home-manager/sharedModules/programs/_1password.nix
@@ -9,20 +9,30 @@
   signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGEXFObvyFbGAgq3Lob/+2SPBXfFBmguTmJDLcJlysJ";
   onePassAgentPath =
     if isLinux
-    then "~/.1password/agent.sock"
+    then "\"~/.1password/agent.sock\""
     else if isDarwin
-    then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else "";
+    then "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
+    else null;
   onePassSshSignPath =
     if isLinux
     then "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}"
     else if isDarwin
     then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-    else "";
+    else null;
 in {
   options = {
     programs._1password = {
       enable = lib.mkEnableOption "1Password";
+      agentPath = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = onePassAgentPath;
+        description = "Path to 1Password agent socket";
+      };
+      sshSignPath = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = onePassSshSignPath;
+        description = "Path to 1Password ssh sign binary";
+      };
     };
   };
 
