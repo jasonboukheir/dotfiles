61822b21d9252dfe929feb935a4bba4ab4c1355f
Jason Bou Kheir <jasonbk@meta.com>
2025-08-12 11:08:53 -0700

    add agentpath and sign path to 1password options

--- modules/home-manager/sharedModules/programs/ssh.nix
+++ modules/home-manager/sharedModules/programs/ssh.nix
@@ -3,22 +3,15 @@
   lib,
   pkgs,
   ...
-}: let
-  onePassAgentPath =
-    if pkgs.stdenv.isLinux
-    then "\"~/.1password/agent.sock\""
-    else if pkgs.stdenv.isDarwin
-    then "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
-    else "";
-in {
+}: {
   config = lib.mkIf config.programs.ssh.enable {
     programs.ssh = {
       matchBlocks = {
         "brutus" = {
           forwardAgent = true;
         };
-        "* !*.od*" = lib.mkIf config.programs._1password.enable {
-          identityAgent = onePassAgentPath;
+        "* !*.od*" = lib.mkIf (config.programs._1password.enable && config.programs._1password.agentPath != null) {
+          identityAgent = config.programs._1password.agentPath;
         };
       };
     };
