e00e43365f20cae09e01de96c5ce1a59f37f472f
Jason Bou Kheir <jasonbk@meta.com>
2025-08-12 10:01:46 -0700

    update ssh hosts in work macbook

--- hosts/work-macbook/home-manager/programs/ssh.nix
+++ hosts/work-macbook/home-manager/programs/ssh.nix
@@ -1,3 +1,13 @@
 {...}: {
-  programs.ssh.enable = true;
+  programs.ssh {
+    enable = true;
+    matchBlocks."*.od*" = {
+      # Use SKS agent for authentication
+            IdentityAgent = "~/.fb-sks-agent/ssh_auth_sock";
+            # Optional: Explicitly specify the SKS-issued certificate
+            IdentityFile = "~/.fb-sks-agent/jasonbk-cert.pub";
+            # Enable adding keys to the agent
+            AddKeysToAgent = "yes";
+    };
+  };
 }
