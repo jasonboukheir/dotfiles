4f9f1c97ceb12c4e4dee7a5b03a512621f07d148
Jason Bou Kheir <jasonbk@meta.com>
2025-08-12 10:07:30 -0700

    add rule for ondemand servers

--- hosts/work-macbook/home-manager/programs/ssh.nix
+++ hosts/work-macbook/home-manager/programs/ssh.nix
@@ -1,13 +1,10 @@
 {...}: {
-  programs.ssh {
+  programs.ssh = {
     enable = true;
     matchBlocks."*.od*" = {
-      # Use SKS agent for authentication
-            IdentityAgent = "~/.fb-sks-agent/ssh_auth_sock";
-            # Optional: Explicitly specify the SKS-issued certificate
-            IdentityFile = "~/.fb-sks-agent/jasonbk-cert.pub";
-            # Enable adding keys to the agent
-            AddKeysToAgent = "yes";
+      identityAgent = "~/.fb-sks-agent/ssh_auth_sock";
+      identityFile = "~/.fb-sks-agent/jasonbk-cert.pub";
+      forwardAgent = true;
     };
   };
 }
