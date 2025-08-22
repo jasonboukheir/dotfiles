2376892927a048dcb2b78e94615483adf6808c8d
Jason Bou Kheir <jasonbk@meta.com>
2025-08-22 14:10:12 -0700

    get vivid module working

--- modules/home-manager/jasonbk/programs/nushell.nix
+++ modules/home-manager/jasonbk/programs/nushell.nix
@@ -1,3 +1,9 @@
-{lib, ...}: {
-  programs.nushell.enable = lib.mkDefault true;
+{lib, ...}: with lib; {
+  programs.nushell = {
+    enable = mkDefault true;
+    vivid = {
+      enable = mkDefault true;
+      theme = mkDefault "nord";
+    };
+  };
 }
---
+++ modules/home-manager/sharedModules/programs/nushell/vivid.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.programs.nushell.vivid;
+in
+
+{
+  options = {
+    programs.nushell.vivid = {
+      enable = mkEnableOption "Enable vivid integration for nushell";
+      pkg = mkOption {
+        type = types.nullOr types.package;
+        default = pkgs.vivid;
+        description = "Package to use for vivid";
+      };
+      theme = mkOption {
+        type = types.str;
+        description = "Theme name to use with vivid generate";
+        example = "gruvbox-dark";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Validation: theme must be set if enabled
+    assertions = [
+      {
+        assertion = cfg.theme != null && cfg.theme != "";
+        message = "programs.nushell.vivid.theme must be set if enable is true";
+      }
+    ];
+
+    home.packages = lib.optional (cfg.pkg != null) cfg.pkg;
+
+    programs.nushell.extraConfig = ''
+      $env.LS_COLORS = (${cfg.pkg}/bin/vivid generate ${cfg.theme})
+    '';
+  };
+}
