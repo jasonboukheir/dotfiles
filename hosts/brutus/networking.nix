ae8a8b4d59407b8896b6d50229a9d163a64b8f51
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-26 19:57:05 -0700

    open port 53

--- hosts/brutus/networking.nix
+++ hosts/brutus/networking.nix
@@ -1,7 +1,7 @@
 {...}: {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [80 443];
+    allowedTCPPorts = [53 80 443];
   };
   networking.networkmanager.enable = true;
   networking.hostName = "brutus"; # Define your hostname.
