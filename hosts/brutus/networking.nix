727a2550240a3c1f91bbd0b842ae4ebed64f40cb
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-08-26 22:04:33 -0700

    get blocky working

--- hosts/brutus/networking.nix
+++ hosts/brutus/networking.nix
@@ -2,6 +2,7 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [53 80 443];
+    allowedUDPPorts = [53];
   };
   networking.networkmanager.enable = true;
   networking.hostName = "brutus"; # Define your hostname.
