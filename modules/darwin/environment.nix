dc5639b6d5c1d5592466772301d7154729554f2f
Jason Bou Kheir <jasonbk@meta.com>
2025-09-09 08:39:26 -0700

    use proper fish package in environment

--- modules/darwin/environment.nix
+++ modules/darwin/environment.nix
@@ -7,6 +7,6 @@
   environment.shells =
     []
     ++ lib.optionals config.programs.bash.enable [pkgs.bash]
-    ++ lib.optionals config.programs.fish.enable [pkgs.fish]
+    ++ lib.optionals config.programs.fish.enable [config.programs.fish.package]
     ++ lib.optionals config.programs.nushell.enable [pkgs.nushell];
 }
