9c997f56d656c7f28f51f0422bd0c4e2fed7b2e2
Jason Bou Kheir <jasonbk@sunnycareboo.com>
2025-09-13 22:13:04 +0100

    configuration is working, just need to build it

--- hosts/pibitcoin/configuration.nix
+++ hosts/pibitcoin/configuration.nix
@@ -4,14 +4,13 @@
   };
   networking = {
     firewall.enable = true;
-    networkmanager.enable = true;
     hostName = "pibitcoin";
   };
   services = {
     openssh = {
       enable = true;
       ports = [22];
-      sesttings = {
+      settings = {
         UseDns = true;
         PasswordAuthentication = false;
         PermitRootLogin = "no";
@@ -22,7 +21,7 @@
     users.jasonbk = {
       isNormalUser = true;
       description = "Jason Bou Kheir";
-      extraGroups = ["networkmanager" "wheel"];
+      extraGroups = ["wheel"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfZvYFG59uHZI+qyuVEyeL6A7GWanxbRbQkQG7q9SWy"
       ];
@@ -33,16 +32,6 @@
     extra-container.enable = true;
   };
   fileSystems = {
-    "/boot/firmware" = {
-      device = "/dev/disk/by-uuid/2178-694E";
-      fsType = "vfat";
-      options = ["noatime" "noauto" "x-systemd.automount" "x-systemd.idle-timeout=1min"];
-    };
-    "/" = {
-      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-      fsType = "ext4";
-      options = ["noatime"];
-    };
     "/var/lib/bitcoind" = {
       device = "/dev/disk/by-uuid/c26b49bd-a64b-4ad5-b762-b613730d7931";
       fsType = "ext4";
--- hosts/pibitcoin/flake.nix
+++ hosts/pibitcoin/flake.nix
@@ -3,16 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-raspberrypi = {
       url = "github:nvmd/nixos-raspberrypi/main";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-  nixConfig = {
-    extra-substituters = [
-      "https://nixos-raspberrypi.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-    ];
   };
 
   outputs = inputs @ {
@@ -20,9 +11,8 @@
     nixpkgs,
     ...
   }: {
-    nixosConfigurations.pibitcoin = nixos-raspberrypi.lib.nixosSystem {
-      system = "aarch64-linux";
-      specialArgs = {inherit inputs;};
+    nixosConfigurations.pibitcoin = nixos-raspberrypi.lib.nixosInstaller {
+      specialArgs = inputs;
       modules = [
         nixos-raspberrypi.nixosModules.raspberry-pi-5.base
         ./configuration.nix
