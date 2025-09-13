f3f9003c4d727dbae5b6cfd94dc7053ce02e8929
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-09-13 09:18:08 -0700

    update pibitcoin flake

--- hosts/pibitcoin/configuration.nix
+++ hosts/pibitcoin/configuration.nix
@@ -3,6 +3,9 @@
   pkgs,
   ...
 }: {
+  system = {
+    stateVersion = "25.05";
+  };
   networking = {
     firewall.enable = true;
     networkmanager.enable = true;
@@ -44,6 +47,23 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfZvYFG59uHZI+qyuVEyeL6A7GWanxbRbQkQG7q9SWy"
       ];
+    };
+  };
+  fileSystems = {
+    "/boot/firmware" = {
+      device = "/dev/disk/by-uuid/2178-694E";
+      fsType = "vfat";
+      options = ["noatime" "noauto" "x-systemd.automount" "x-systemd.idle-timeout=1min"];
+    };
+    "/" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+    "/var/lib/bitcoind" = {
+      device = "/dev/disk/by-uuid/c26b49bd-a64b-4ad5-b762-b613730d7931";
+      fsType = "ext4";
+      options = ["noatime"];
     };
   };
 }
