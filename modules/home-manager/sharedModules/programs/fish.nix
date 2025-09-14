34ab0d648b3f5ec9e63f1d517806b3f65947d699
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-09-14 16:20:44 -0700

    setup nord theme in fish shell

--- modules/home-manager/sharedModules/programs/fish.nix
+++ modules/home-manager/sharedModules/programs/fish.nix
@@ -7,12 +7,17 @@
   config = lib.mkIf config.programs.fish.enable {
     home.shell.enableFishIntegration = lib.mkDefault true;
     programs = {
-      fish.plugins = [
-        {
-          name = "plugin-git";
-          src = pkgs.fishPlugins.plugin-git.src;
-        }
-      ];
+      fish = {
+        shellInit = ''
+          fish_config theme choose Nord
+        '';
+        plugins = [
+          {
+            name = "plugin-git";
+            src = pkgs.fishPlugins.plugin-git.src;
+          }
+        ];
+      };
     };
   };
 }
