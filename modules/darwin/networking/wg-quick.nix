25dae8a5a22d99de6914b0b5948958055e0feaba
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-09-13 20:48:23 -0700

    add wg interface to macos

--- modules/darwin/networking/wg-quick.nix
+++ modules/darwin/networking/wg-quick.nix
@@ -10,7 +10,7 @@
   config = lib.mkIf config.networking.wg-quick.homelab.enable {
     networking.wg-quick.interfaces.wg0 = {
       # Client's VPN IP/subnet
-      address = ["10.0.0.3/32"];
+      address = ["10.100.0.3/32"];
 
       # Path to private key (ensure it's readable by root/systemd)
       privateKeyFile = "/var/lib/wireguard/wg0.key";
@@ -18,7 +18,7 @@
       peers = [
         {
           publicKey = "VxT8i+moMUcUbLtmHcdyb5l/t6G+KI57k+2IoKhoMjU=";
-          allowedIPs = ["10.0.0.0/24" "192.168.1.0/24"]; # VPN subnet + home LAN subnet for relay access
+          allowedIPs = ["10.100.0.0/24" "192.168.50.0/24"]; # VPN subnet + home LAN subnet for relay access
           endpoint = "50.47.248.79:51820";
           persistentKeepalive = 25; # Optional for NAT traversal
         }
