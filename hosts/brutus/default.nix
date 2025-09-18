faf7e286caaedcfcbd817dea76d1161bdc16dde6
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-09-18 16:59:47 -0700

    format disk with disko

--- hosts/brutus/default.nix
+++ hosts/brutus/default.nix
@@ -6,7 +6,7 @@
     ./services
     ./systemd
     ./configuration.nix
-    #    ./disko-configuration.nix
+    ./disko-configuration.nix
     ./hardware-configuration.nix
     ./nixpkgs.nix
     ./networking
