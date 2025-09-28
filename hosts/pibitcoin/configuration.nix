8f0470251d3fe62648e0ac3e5c485ab8c8f3e3a2
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-09-27 20:26:11 -0700

    add nix.settings

--- hosts/pibitcoin/configuration.nix
+++ hosts/pibitcoin/configuration.nix
@@ -2,6 +2,7 @@
   system = {
     stateVersion = "25.05";
   };
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   networking = {
     firewall.enable = true;
     hostName = "pibitcoin";
