a5fa0afe2ec1d8913b04f6fbd7e6acc920eaf3ca
Jason Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-04 04:55:18 +0000

    mount both nvme and sdcard also in mnt directory

--- hosts/pibitcoin/default.nix
+++ hosts/pibitcoin/default.nix
@@ -40,5 +40,26 @@
       options = ["noatime"];
       neededForBoot = true;
     };
+    "/var/lib/bitcoind" = {
+      depends = [
+        "/"
+        "/nix"
+      ];
+      device = "/nix";
+      fsType = "none";
+      options = [
+        "bind"
+      ];
+    };
+    "/mnt/sdCard" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+      options = ["defaults"];
+    };
+    "/mnt/nvme" = {
+      device = "/dev/disk/by-uuid/c26b49bd-a64b-4ad5-b762-b613730d7931";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
   };
 }
