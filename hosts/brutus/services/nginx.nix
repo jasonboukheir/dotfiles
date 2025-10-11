31065ace7f401b0284190ad7b860a73e723c752e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-11 07:46:58 -0700

    refactor nginx services to just be a mapping from subdomain to config

--- hosts/brutus/services/nginx.nix
+++ hosts/brutus/services/nginx.nix
@@ -1,139 +1,44 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  services = {
+    ai = {port = 3100;};
+    blocky = {port = 1501;};
+    budget = {port = 3000;};
+    "litellm.ai" = {port = 3200;};
+    "pocket-id" = {port = 1411;};
+    jellyfin = {port = 8096;};
+
+    # nixarr
+    transmission = {port = 9091;};
+    bazarr = {port = 6767;};
+    lidarr = {port = 8686;};
+    prowlarr = {port = 9696;};
+    radarr = {port = 7878;};
+    readarr = {port = 8787;};
+    sonarr = {port = 8989;};
+    jellyseerr = {port = 5055;};
+  };
+  mkVirtualHost = subdomain: config:
+    lib.nameValuePair "${subdomain}.sunnycareboo.com" {
+      forceSSL = true;
+      enableACME = true;
+      acmeRoot = null;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.port}";
+        proxyWebsockets = true;
+      };
+    };
+in {
   services.nginx = {
     enable = true;
     recommendedProxySettings = true; # Optional but good for reverse proxies
     recommendedTlsSettings = true; # Optional for better TLS defaults
 
-    virtualHosts = {
-      "blocky.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:1501"; # Proxies to your blocky instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-      "budget.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:3000"; # Proxies to your blocky instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-      "ai.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:3100"; # Proxies to your blocky instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-      "litellm.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:3200"; # Proxies to your blocky instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-      "pocket-id.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:1411"; # Proxies to your pocket-id instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-      "jellyfin.sunnycareboo.com" = {
-        forceSSL = true; # Redirects HTTP to HTTPS
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:8096"; # Proxies to your jellyfin instance
-          proxyWebsockets = true; # If needed for WebSocket support; adjust as necessary
-        };
-      };
-
-      # nixarr
-      "transmission.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://0.0.0.0:9091";
-          proxyWebsockets = true;
-        };
-      };
-      "bazarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:6767";
-          proxyWebsockets = true;
-        };
-      };
-      "lidarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:8686";
-          proxyWebsockets = true;
-        };
-      };
-      "prowlarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:9696";
-          proxyWebsockets = true;
-        };
-      };
-      "radarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:7878";
-          proxyWebsockets = true;
-        };
-      };
-      "readarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:8787";
-          proxyWebsockets = true;
-        };
-      };
-      "sonarr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:8989";
-          proxyWebsockets = true;
-        };
-      };
-      "jellyseerr.sunnycareboo.com" = {
-        forceSSL = true;
-        enableACME = true;
-        acmeRoot = null;
-        locations."/" = {
-          proxyPass = "http://localhost:5055";
-          proxyWebsockets = true;
-        };
-      };
-    };
+    virtualHosts =
+      lib.mapAttrs' mkVirtualHost services;
   };
 
   security.acme = {
