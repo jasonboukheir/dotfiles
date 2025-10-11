49b3eceb257bb4b60bbc0edabdf3353a309d35cf
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-11 13:44:38 -0700

    make litellm work

--- hosts/brutus/services/litellm.nix
+++ hosts/brutus/services/litellm.nix
@@ -1,13 +1,49 @@
-{...}: {
-  services.litellm = {
-    enable = true;
-    port = 3200;
-    environment = {
-      "DATABASE_USER" = "litellm";
-      "DATABASE_PORT" = "5432";
-      "DATABASE_HOST" = "127.0.0.1";
-      "DATABASE_NAME" = "litellm";
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  virtualisation.oci-containers.containers = {
+    litellm = {
+      autoStart = true;
+      image = "berriai/litellm:main-stable";
+      cmd = [
+        "--port=3200"
+        "--host=localhost"
+      ];
+      environment = {
+        "DATABASE_URL" = "postgresql://litellm@localhost:5432/litellm";
+      };
+      environmentFiles = [
+        "/var/lib/secrets/liteLlmSecrets"
+      ];
+      extraOptions = [
+        "--network=host"
+      ];
     };
-    environmentFile = "/var/lib/secrets/liteLlmSecrets";
   };
+  services.postgresql = {
+    ensureUsers = [
+      {
+        name = "litellm";
+        ensureDBOwnership = true;
+      }
+    ];
+    ensureDatabases = [
+      "litellm"
+    ];
+  };
+  # services.litellm = {
+  #   enable = true;
+  #   port = 3200;
+  #   environment = {
+  #     "DATABASE_URL" = "postgresql://litellm@localhost:5432/litellm";
+  #   };
+  #   environmentFile = "/var/lib/secrets/liteLlmSecrets";
+  # };
+  # systemd.services.litellm = {
+  #   serviceConfig = {
+  #     ExecStartPre = "${lib.getExe pkgs.prisma} generate --schema ${pkgs.litellm}/lib/python3.13/site-packages/litellm/proxy/schema.prisma";
+  #   };
+  # };
 }
