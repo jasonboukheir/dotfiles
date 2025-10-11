d4f49c342e7c96ed343e35fb38f98823d6260948
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-11 08:24:01 -0700

    setup litellm and postgresql

--- hosts/brutus/services/litellm.nix
+++ hosts/brutus/services/litellm.nix
@@ -1,6 +1,13 @@
 {...}: {
   services.litellm = {
-    enable = false; # need to enable postgresql later
+    enable = true;
     port = 3200;
+    environment = {
+      "DATABASE_USER" = "litellm";
+      "DATABASE_PORT" = "5432";
+      "DATABASE_HOST" = "127.0.0.1";
+      "DATABASE_NAME" = "litellm";
+    };
+    environmentFile = "/var/lib/secrets/liteLlmSecrets";
   };
 }
