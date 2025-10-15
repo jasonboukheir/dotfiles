4e06a921ec8acba8621842eb8f5802fe2a658212
Jason Bou Kheir <jasonbk@meta.com>
2025-10-15 06:39:56 -0700

    add brave to dock

--- hosts/work-macbook/system/dock.nix
+++ hosts/work-macbook/system/dock.nix
@@ -1,7 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   system.defaults.dock = {
     persistent-apps = [
       "/Applications/Google Chrome.app"
+      "${pkgs.brave}/Applications/Brave Browser.app"
       "/Applications/Ghostty.app"
       "/Applications/VS Code @ FB.app"
     ];
