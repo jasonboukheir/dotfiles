a98d57a754749488a8c10b11bd75b087855c0d29
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-14 21:01:46 -0700

    add mealie

---
+++ hosts/brutus/services/mealie.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.services.mealie;
+  domain = "meals.sunnycareboo.com";
+in {
+  services.mealie = {
+    enable = true;
+    port = 9000;
+    settings = {
+      "OIDC_AUTH_ENABLED" = "True";
+      "OIDC_SIGNUP_ENABLED" = "True";
+      "OIDC_CONFIGURATION_URL" = "https://pocket-id.sunnycareboo.com/.well-known/openid-configuration";
+      "OIDC_CLIENT_ID" = "32cb542a-a0b1-4278-8cfa-0dd5d9a2020d";
+      "OIDC_ADMIN_GROUP" = "admin";
+      "OIDC_PROVIDER_NAME" = "Pocket ID";
+    };
+    database.createLocally = true;
+    extraOptions = [];
+    credentialsFile = "/var/lib/secrets/mealie.env";
+    listenAddress = "0.0.0.0";
+  };
+
+  services.nginx.virtualHosts."${domain}" = lib.mkIf cfg.enable {
+    forceSSL = true;
+    enableACME = true;
+    acmeRoot = null;
+    locations."/" = {
+      proxyWebsockets = true;
+      proxyPass = "http://localhost:${toString cfg.port}";
+    };
+  };
+}
---
+++ hosts/brutus/services/seafile.nix
@@ -0,0 +1,7 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  # TODO: get this working!
+}
