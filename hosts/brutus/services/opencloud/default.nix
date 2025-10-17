19412342d1b651fca41ca31203bf6f852bb4572c
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-10-16 17:57:52 -0700

    get opencloud working

--- hosts/brutus/services/opencloud/default.nix
+++ hosts/brutus/services/opencloud/default.nix
@@ -1,15 +1,22 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.services.opencloud;
   domain = "cloud.sunnycareboo.com";
+  url = "https://${domain}";
+  oidc_domain = "pocket-id.sunnycareboo.com";
+  oidc_url = "https://${oidc_domain}";
+  cspConfigFile =
+    pkgs.writeText "opencloud-csp-config"
+    (builtins.readFile ./csp.yaml);
 in {
   # TODO: setup pocket-id as idp
   services.opencloud = {
-    enable = false;
-    url = "https://${domain}";
+    enable = true;
+    url = url;
     address = "127.0.0.1";
     port = 9200;
     stateDir = "/var/lib/opencloud";
@@ -61,11 +68,16 @@ in {
       "INITIAL_ADMIN_PASSWORD" = "admin";
 
       # oidc
-      "OC_OIDC_ISSUER" = "https://pocket-id.sunnycareboo.com";
-      "WEB_OPTION_ACCOUNT_EDIT_LINK_HREF" = "https://pocket-id.sunnycareboo.com";
+      "OC_ADMIN_USER_ID" = "";
+      "OC_OIDC_CLIENT_ID" = "4c4ddfb4-b892-4563-9f7f-80cad38fd084";
+      "OC_OIDC_ISSUER" = oidc_url;
+      "WEB_OPTION_ACCOUNT_EDIT_LINK_HREF" = "${oidc_url}/settings/account";
       "OC_EXCLUDE_RUN_SERVICES" = "idp";
       "GRAPH_USERNAME_MATCH" = "none";
       "GRAPH_ASSIGN_DEFAULT_USER_ROLE" = "false";
+
+      # csp settings
+      "PROXY_CSP_CONFIG_FILE_LOCATION" = "${cspConfigFile}";
     };
     environmentFile = "/var/lib/secrets/opencloud.env";
   };
