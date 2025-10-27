2100c9c1d53ccbe61ec987d9a6e202d7f49620a8
Jason Bou Kheir <jasonbk@meta.com>
2025-10-27 11:33:25 -0700

    fix hosts in ssh

--- modules/home-manager/sharedModules/programs/ssh.nix
+++ modules/home-manager/sharedModules/programs/ssh.nix
@@ -4,6 +4,16 @@
   pkgs,
   ...
 }: {
+  options = {
+    _1passwordSshHostGlob = lib.mkOption {
+      type = lib.types.str;
+      default = "*";
+      description = ''
+        When using 1password as ssh agent, what glob pattern should be used as host match?
+      '';
+      example = lib.literalExpression "* !*.od*";
+    };
+  };
   config = lib.mkIf config.programs.ssh.enable {
     programs.ssh = {
       enableDefaultConfig = false;
@@ -14,7 +24,7 @@
         "pibitcoin" = {
           forwardAgent = true;
         };
-        "* !*.od*" = lib.mkIf (config.programs._1password.enable && config.programs._1password.agentPath != null) {
+        "${config._1passwordSshHostGlob}" = lib.mkIf (config.programs._1password.enable && config.programs._1password.agentPath != null) {
           identityAgent = config.programs._1password.agentPath;
         };
       };
