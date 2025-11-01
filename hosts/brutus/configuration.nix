596be35aaf313d2eead8ce7e60f14b26fb995544
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-11-01 08:36:07 -0700

    add initial agenix secrets config

--- hosts/brutus/configuration.nix
+++ hosts/brutus/configuration.nix
@@ -48,4 +48,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+
+  environment.variables.EDITOR = "nvim";
 }
