ed73978359ceaae972aff8b603badc53c4268791
Jason Bou Kheir <jasonbk@meta.com>
2025-11-10 08:04:21 -0800

    fix zed

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   stylix = {
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     # image = ./judgement.jpg;
-    base16Scheme = ./orthodox-dark.yaml;
+    # base16Scheme = ./orthodox-dark.yaml;
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.fira-code;
