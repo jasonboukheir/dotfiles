8fd3594404fe04f2af756ed30e8fc762e35e0d5c
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-11-22 23:43:27 -0800

    fixup settings

--- hosts/litus/configuration.nix
+++ hosts/litus/configuration.nix
@@ -1,8 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{ ... }:
-{
+{...}: {
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
