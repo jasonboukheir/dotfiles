69e696f0899b90acab6b75c279dfa46f8fdad4c3
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-11-25 00:18:26 -0800

    set sunnycareboo.com in blocky with local ip

--- hosts/brutus/services/blocky.nix
+++ hosts/brutus/services/blocky.nix
@@ -9,27 +9,7 @@ in {
     enable = true;
     settings = {
       ports = {
-        dns = 53;
         http = 1501;
-      };
-      upstreams.groups.default = [
-        "https://one.one.one.one/dns-query"
-      ];
-      bootstrapDns = {
-        upstream = "https://one.one.one.one/dns-query";
-        ips = ["1.1.1.1" "1.0.0.1"];
-      };
-      blocking = {
-        blackLists = {
-          ads = [
-            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-            "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/hosts/pro.txt"
-          ];
-          adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
-        };
-        clientGroupsBlock = {
-          default = ["ads" "adult"];
-        };
       };
     };
   };
--- hosts/litus/services/blocky.nix
+++ hosts/litus/services/blocky.nix
@@ -1,30 +1,3 @@
 {...}: {
-  services.blocky = {
-    enable = true;
-    settings = {
-      ports = {
-        dns = 53;
-        http = 1501;
-      };
-      upstreams.groups.default = [
-        "https://one.one.one.one/dns-query"
-      ];
-      bootstrapDns = {
-        upstream = "https://one.one.one.one/dns-query";
-        ips = ["1.1.1.1" "1.0.0.1"];
-      };
-      blocking = {
-        blackLists = {
-          ads = [
-            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-            "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/hosts/pro.txt"
-          ];
-          adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
-        };
-        clientGroupsBlock = {
-          default = ["ads" "adult"];
-        };
-      };
-    };
-  };
+  services.blocky.enable = true;
 }
