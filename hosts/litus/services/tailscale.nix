d62f5babb3c66a6d2934f502234d971cce9ba61c
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-11-29 17:55:44 -0800

    get litus tailscale setup

--- hosts/litus/services/tailscale.nix
+++ hosts/litus/services/tailscale.nix
@@ -10,7 +10,8 @@ in {
     openFirewall = true;
     authKeyFile = config.age.secrets."tailscale/authkey".path;
     authKeyParameters = {
-      baseURL = "https://${config.sunnycareboo.services.headscale.domain}";
+      baseURL = "https://headscale.sunnycareboo.com";
+      preauthorized = true;
     };
   };
   age.secrets."tailscale/authkey".file = ../secrets/tailscale/authkey.age;
