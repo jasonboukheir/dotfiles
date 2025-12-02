63620e2eb9f915e55c17b1ba56fb8ca6ac6bd103
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-12-01 22:31:46 -0800

    fixup nvidia

--- hosts/thebeast/nvidia.nix
+++ hosts/thebeast/nvidia.nix
@@ -9,6 +9,7 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = false;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
