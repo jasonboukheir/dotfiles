7c0716ff989f4cfa5d6002c05757b732b72d188a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-12-07 16:47:56 -0800

    switch to kde plasma (#3)
    * try using kde plasma
    
    * remove omarchy type services that are not needed

--- hosts/thebeast/default.nix
+++ hosts/thebeast/default.nix
@@ -1,15 +1,18 @@
 {...}: {
   imports = [
     ./home-manager
+    ./audio.nix
+    ./bluetooth.nix
     ./configuration.nix
+    ./desktop.nix
     ./graphics.nix
     ./hardware-configuration.nix
     ./jovian.nix
+    ./networking.nix
     ./nixpkgs.nix
-    ./omarchy.nix
+    ./programs.nix
     ./stylix.nix
     ./../../modules
     ./../../modules/nixos
-    ./../../modules/omarchy
   ];
 }
---
+++ hosts/thebeast/networking.nix
@@ -0,0 +1,6 @@
+{...}: {
+  networking = {
+    hostName = "thebeast";
+    networkmanager.enable = true;
+  };
+}
