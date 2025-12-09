5d26391b464c201af7660ac89faa536a53882d05
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-12-08 23:41:54 -0800

    add nordzy icons

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -3,8 +3,8 @@
     image = ./wallpapers/nord.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     cursor = {
-      name = "Capitaine Cursors (Nord)";
-      package = pkgs.capitaine-cursors-themed;
+      name = "Nordzy-cursors";
+      package = pkgs.nordzy-cursor-theme;
       size = 18;
     };
     fonts = {
@@ -12,6 +12,12 @@
         package = pkgs.nerd-fonts.fira-code;
         name = "FiraCode Nerd Font";
       };
+    };
+    icons = {
+      enable = true;
+      package = pkgs.nordzy-icon-theme;
+      light = "Nordzy";
+      dark = "Nordzy-dark";
     };
   };
 }
