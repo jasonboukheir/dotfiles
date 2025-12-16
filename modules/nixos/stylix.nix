50ee03196161b74de1b1bf6f3efb6300c17ed1f6
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2025-12-16 02:11:18 -0800

    fix stylix syntax for nixos

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,13 +1,15 @@
 {pkgs, ...}: {
-  cursor = {
-    name = "Capitaine Cursors (Nord)";
-    package = pkgs.capitaine-cursors-themed;
-    size = 18;
-  };
-  icons = {
-    enable = true;
-    package = pkgs.nordzy-icon-theme;
-    light = "Nordzy";
-    dark = "Nordzy-dark";
+  stylix = {
+    cursor = {
+      name = "Capitaine Cursors (Nord)";
+      package = pkgs.capitaine-cursors-themed;
+      size = 18;
+    };
+    icons = {
+      enable = true;
+      package = pkgs.nordzy-icon-theme;
+      light = "Nordzy";
+      dark = "Nordzy-dark";
+    };
   };
 }
