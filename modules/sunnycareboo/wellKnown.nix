2a0f55c421caeaeaa9938eba9b08699ec48ca164
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-01-28 20:33:08 -0800

    update SRV records on blocky

---
+++ modules/sunnycareboo/wellKnown.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.sunnycareboo;
+in {
+  options.sunnycareboo.wellKnown = {
+    caldav = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Domain of the caldav server.";
+    };
+    carddav = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Domain of the carddav server.";
+    };
+    webdav = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Domain of the webdav server.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.nginx.virtualHosts.${cfg.baseDomain}.locations = {
+      "/.well-known/caldav" = mkIf (cfg.wellKnown.caldav != null) {
+        return = "301 https://${cfg.wellKnown.caldav}";
+      };
+      "/.well-known/carddav" = mkIf (cfg.wellKnown.carddav != null) {
+        return = "301 https://${cfg.wellKnown.carddav}";
+      };
+      "/remote.php/dav" = mkIf (cfg.wellKnown.webdav != null) {
+        return = "301 https://${cfg.wellKnown.webdav}/remote.php/dav";
+      };
+    };
+  };
+}
