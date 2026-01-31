d03740e51d3346fafcc1f0408edb09fc437c93dc
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-01-30 21:45:12 -0800

    disable hydroxide, just use web client

--- hosts/thebeast/programs.nix
+++ hosts/thebeast/programs.nix
@@ -2,7 +2,7 @@
   programs = {
     _1password.enable = true;
     _1password-gui.enable = true;
-    thunderbird.enable = true;
+    thunderbird.enable = false;
   };
   environment.systemPackages = with pkgs; [
     lutris
