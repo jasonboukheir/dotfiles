0cc44d952f6d6cfcad1791c9154026e7edef468a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-02-12 22:32:17 -0800

    set polarity in stylix

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -2,6 +2,7 @@
   stylix = {
     image = ./wallpapers/nord.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    polarity = "dark";
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.fira-code;
