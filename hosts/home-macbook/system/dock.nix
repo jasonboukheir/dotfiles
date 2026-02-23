0f15b6b3ad826bbc5ec2e2663a581b2ba0e286f8
Jason Bou Kheir <jasonbk@meta.com>
2026-02-23 10:16:11 -0800

    use ghostty-bin on darwin

--- hosts/home-macbook/system/dock.nix
+++ hosts/home-macbook/system/dock.nix
@@ -2,7 +2,7 @@
   system.defaults.dock = {
     persistent-apps = [
       "${pkgs.brave}/Applications/Brave Browser.app"
-      "/Applications/Ghostty.app"
+      "${pkgs.ghostty-bin}/Applications/Ghostty.app"
       "/System/Applications/Messages.app"
       "/System/Applications/Mail.app"
       "/System/Applications/Music.app"
--- hosts/work-macbook/system/dock.nix
+++ hosts/work-macbook/system/dock.nix
@@ -3,7 +3,7 @@
     persistent-apps = [
       "/Applications/Google Chrome.app"
       "${pkgs.brave}/Applications/Brave Browser.app"
-      "/Applications/Ghostty.app"
+      "${pkgs.ghostty-bin}/Applications/Ghostty.app"
       "/Applications/VS Code @ FB.app"
     ];
   };
