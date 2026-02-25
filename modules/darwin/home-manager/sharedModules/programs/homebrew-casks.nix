3723a3cdaa775f906ee9a6590e9c197b684822d3
Jason Bou Kheir <jasonbk@meta.com>
2026-02-25 15:34:58 -0800

    fix zeditor cli:

--- modules/darwin/home-manager/sharedModules/programs/homebrew-casks.nix
+++ modules/darwin/home-manager/sharedModules/programs/homebrew-casks.nix
@@ -8,11 +8,21 @@
     zed = "zed-editor";
   };
 
+  # Programs that use extraPackages for wrapping (unnecessary on darwin
+  # since tools are already in PATH via home.packages / programs.*.enable).
+  programsWithExtraPackages = ["zed-editor"];
+
   caskCfg = osConfig.homebrewCasks;
 in {
   config = lib.mkMerge (lib.mapAttrsToList (caskName: programName:
     lib.mkIf caskCfg.${caskName}.enable {
-      programs.${programName}.package = caskCfg.${caskName}.package;
+      programs.${programName} =
+        {
+          package = caskCfg.${caskName}.package;
+        }
+        // lib.optionalAttrs (builtins.elem programName programsWithExtraPackages) {
+          extraPackages = lib.mkForce [];
+        };
     })
   caskToProgram);
 }
