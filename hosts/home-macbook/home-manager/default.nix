878c37dd0c4e345af947a8e107be7ceb8d85b2fa
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-02-26 08:25:53 -0800

    update to unstable claude and gemini

--- hosts/home-macbook/home-manager/default.nix
+++ hosts/home-macbook/home-manager/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
     ./users
+    ./extraSpecialArgs.nix
   ];
 }
--- hosts/home-macbook/home-manager/users/jasonbk/programs/default.nix
+++ hosts/home-macbook/home-manager/users/jasonbk/programs/default.nix
@@ -1,10 +1,12 @@
-{...}: {
+{pkgs-unstable, ...}: {
   programs = {
     _1password.enable = true;
     brave.enable = true;
     claude-code.enable = true;
+    claude-code.package = pkgs-unstable.claude-code;
     fish.enable = true;
     gemini-cli.enable = true;
+    gemini-cli.package = pkgs-unstable.gemini-cli;
     ghostty.enable = true;
     nushell.enable = true;
     ssh.enable = true;
