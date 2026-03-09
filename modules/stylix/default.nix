cc0e62cf5dce735017e7894a63ddb483c18e3947
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-03-09 16:21:08 -0700

    change polarity to light for a light theme like analog dreams

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -3,7 +3,7 @@
     image = ./wallpapers/vaporwave-dolphins.jpg;
     # base16Scheme = ./themes/eastern-orthodox.yaml;
     base16Scheme = ./themes/analog-dreams.yaml;
-    polarity = "dark";
+    polarity = "light";
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.fira-code;
