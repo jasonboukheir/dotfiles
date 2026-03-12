eb55116f7cd29b5c6281be3e9a547321e44fcf4e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-03-12 16:09:54 -0700

    fixup keybindings in zed editor

--- hosts/thebeast/home-manager/jasonbk/programs/zed-editor.nix
+++ hosts/thebeast/home-manager/jasonbk/programs/zed-editor.nix
@@ -2,5 +2,30 @@
   programs.zed-editor = {
     enable = true;
     userSettings.window.titlebar = "custom";
+    userKeymaps = [
+      {
+        context = "Editor";
+        bindings = {
+          "ctrl-c" = "editor::Copy";
+          "ctrl-x" = "editor::Cut";
+          "ctrl-v" = "editor::Paste";
+          "ctrl-z" = "editor::Undo";
+          "ctrl-shift-z" = "editor::Redo";
+          "ctrl-a" = "editor::SelectAll";
+          "ctrl-s" = "workspace::Save";
+          "ctrl-f" = "search::Deploy";
+          "ctrl-w" = "pane::CloseActiveItem";
+          "ctrl-p" = "file_finder::Toggle";
+          "ctrl-shift-p" = "command_palette::Toggle";
+        };
+      }
+      {
+        context = "Terminal";
+        bindings = {
+          "ctrl-shift-c" = "terminal::Copy";
+          "ctrl-shift-v" = "terminal::Paste";
+        };
+      }
+    ];
   };
 }
