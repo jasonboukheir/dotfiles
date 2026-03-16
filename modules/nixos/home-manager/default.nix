1f839cacbb78888dcdd711d6cb818fc029b9985c
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-03-16 00:46:24 -0700

    make it possible to import stylix correctly

--- modules/nixos/home-manager/default.nix
+++ modules/nixos/home-manager/default.nix
@@ -1,5 +1,9 @@
-{...}: {
-  home-manager.sharedModules = [
+{
+  lib,
+  config,
+  ...
+}: {
+  home-manager.sharedModules = lib.optional config.stylix.enable [
     {stylix.targets.qt.platform = "qtct";}
   ];
   imports = [
