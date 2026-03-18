66bb3de97db9a8e6e4409f6cadbcafdd4ef61372
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-03-17 22:08:38 -0700

    revert tty changes

--- hosts/thebeast/specialisations/dev/greetd.nix
+++ hosts/thebeast/specialisations/dev/greetd.nix
@@ -2,11 +2,8 @@
   tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
   hyprlandSessions = "${pkgs.hyprland}/share/wayland-sessions";
 in {
-  boot.kernelParams = ["console=tty1"];
-
   services.greetd = {
     enable = true;
-    vt = 2;
     settings = {
       default_session = {
         command = "${tuigreet} --time --remember --remember-session --sessions ${hyprlandSessions}";
