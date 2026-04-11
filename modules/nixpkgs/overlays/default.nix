48e74fce3cb52efb58955bf7e61570aac92f81f8
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-11 12:59:23 -0700

    fixup fish on darwin

--- modules/nixpkgs/overlays/default.nix
+++ modules/nixpkgs/overlays/default.nix
@@ -1,6 +1,7 @@
-{...}: {
+{inputs, ...}: {
   nixpkgs.overlays = [
     (import ./direnv.nix)
+    (import ./fish.nix {inherit inputs;})
     (import ./lidarr.nix)
     (import ./pocket-id.nix)
   ];
