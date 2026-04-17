39a772e48b7d9a33225322203a4066221da3d1fd
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-16 17:12:59 -0700

    fixup et and zmx on home-macbook too

--- hosts/home-macbook/home-manager/users/jasonbk/programs/default.nix
+++ hosts/home-macbook/home-manager/users/jasonbk/programs/default.nix
@@ -6,11 +6,9 @@
     _1password.enable = true;
     brave.enable = true;
     claude-code.enable = true;
-    eternal-terminal.enable = true;
     fish.enable = true;
     gemini-cli.enable = true;
     ghostty.enable = true;
-    zmx.enable = true;
     nushell.enable = true;
     ssh.enable = true;
     zed-editor.enable = true;
