82dd43c9b872a24f4331271517fbac7d2f22ca09
Jason Bou Kheir <jasonbk@fb.com>
2026-04-20 14:02:37 -0700

    fixup work devserver env

--- hosts/work-devserver/default.nix
+++ hosts/work-devserver/default.nix
@@ -1,4 +1,37 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  proxyUrl = "http://localhost:18082";
+
+  nixProxyWrapper = {
+    bash = ''
+      nix() {
+        http_proxy=${proxyUrl} https_proxy=${proxyUrl} command nix "$@"
+      }
+    '';
+    fish = ''
+      function nix --wraps nix
+        set -lx http_proxy ${proxyUrl}
+        set -lx https_proxy ${proxyUrl}
+        command nix $argv
+      end
+    '';
+    nushell = ''
+      def --wrapped nix [...rest] {
+        $env.http_proxy = "${proxyUrl}"
+        $env.https_proxy = "${proxyUrl}"
+        ^nix ...$rest
+      }
+    '';
+    zsh = ''
+      nix() {
+        http_proxy=${proxyUrl} https_proxy=${proxyUrl} command nix "$@"
+      }
+    '';
+  };
+in {
   imports = [
     ../../modules/home-manager/sharedModules/programs
     ../../modules/home-manager/jasonbk/programs
@@ -15,10 +48,50 @@
       fd
       ripgrep
       ripgrep-all
+      (writeShellScriptBin "cws" (builtins.readFile ./cws.sh))
     ];
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
   };
 
   programs = {
     zmx.enable = true;
+
+    bash = {
+      enable = true;
+      enableCompletion = false;
+      historyFileSize = -1;
+      historySize = 1000000;
+      shellOptions = ["histappend"];
+      bashrcExtra = ''
+        source /etc/bashrc
+        source /usr/facebook/ops/rc/master.bashrc
+      '';
+      initExtra = nixProxyWrapper.bash;
+    };
+
+    fish.interactiveShellInit = lib.mkAfter nixProxyWrapper.fish;
+
+    nushell.extraConfig = lib.mkAfter nixProxyWrapper.nushell;
+
+    zsh = {
+      enable = true;
+      enableCompletion = false;
+      history = {
+        size = 1000000;
+        save = 1000000;
+        append = true;
+      };
+      initContent = lib.mkMerge [
+        (lib.mkBefore ''
+          source /usr/facebook/ops/rc/master.zshrc
+        '')
+        (lib.mkAfter (''
+          setopt COMPLETE_IN_WORD
+        '' + nixProxyWrapper.zsh))
+      ];
+    };
   };
 }
