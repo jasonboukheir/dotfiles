abd9b5aef5c1032b1bfccc575b8f3fb1fdc63568
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-20 22:11:10 -0700

    really fix ups

--- hosts/brutus/power/ups.nix
+++ hosts/brutus/power/ups.nix
@@ -29,7 +29,14 @@
     };
   };
 
-  systemd.services.upsdrv.after = lib.mkForce [ "network.target" ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="09ae", ATTR{idProduct}=="3016", TAG+="systemd", ENV{SYSTEMD_ALIAS}="/dev/ups-tripplite"
+  '';
+
+  systemd.services.upsdrv = {
+    after = lib.mkForce [ "network.target" "dev-ups\\x2dtripplite.device" ];
+    wants = [ "dev-ups\\x2dtripplite.device" ];
+  };
 
   systemd.services.upsd.after = lib.mkForce [
     "network.target"
