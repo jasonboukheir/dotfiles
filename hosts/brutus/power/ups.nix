503c5d962860bb5ce6e722b0257d71a745e3cab3
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-20 22:09:47 -0700

    fix ups failures

--- hosts/brutus/power/ups.nix
+++ hosts/brutus/power/ups.nix
@@ -1,10 +1,9 @@
-{config, ...}: {
+{config, lib, ...}: {
   age.secrets."power/ups/user/pw".file = ../secrets/power/ups/user/pw.age;
   power.ups = {
     enable = true;
     mode = "standalone";
 
-    # 1. Define the UPS Device (The Hardware)
     ups.tripplite = {
       driver = "usbhid-ups";
       port = "auto";
@@ -16,15 +15,11 @@
       maxStartDelay = null;
     };
 
-    # 2. Define the User (The Software Access)
-    # NUT requires a local user to monitor the device.
     users.nut = {
       passwordFile = config.age.secrets."power/ups/user/pw".path;
-      upsmon = "primary"; # This machine is in charge of the UPS
+      upsmon = "primary";
     };
 
-    # 3. Define the Monitor (The Logic)
-    # This watches the UPS defined above ('tripplite@localhost')
     upsmon.monitor.tripplite = {
       system = "tripplite@localhost";
       powerValue = 1;
@@ -33,4 +28,16 @@
       type = "primary";
     };
   };
+
+  systemd.services.upsdrv.after = lib.mkForce [ "network.target" ];
+
+  systemd.services.upsd.after = lib.mkForce [
+    "network.target"
+    "upsdrv.service"
+  ];
+
+  systemd.services.upsmon.after = lib.mkForce [
+    "network.target"
+    "upsd.service"
+  ];
 }
