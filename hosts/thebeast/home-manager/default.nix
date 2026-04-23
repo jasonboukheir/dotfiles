072a5da15ecc2b7ff3c458bd91e8303550a8940b
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-23 00:31:56 -0700

    fix home-manager startup for gamer

--- hosts/thebeast/home-manager/default.nix
+++ hosts/thebeast/home-manager/default.nix
@@ -1,7 +1,11 @@
-{...}: {
+{lib, config, ...}: {
   imports = [
     ./sharedModules
   ];
   home-manager.users.jasonbk.imports = [./jasonbk];
-  home-manager.users.gamer.imports = [./gamer];
+  home-manager.users.gamer.imports =
+    [./gamer]
+    ++ lib.optionals config.stylix.enable [
+      {stylix.enable = false;}
+    ];
 }
