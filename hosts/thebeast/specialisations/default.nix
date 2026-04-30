6cbfe619db280892ff638e71d1af456dcc9907f1
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-04-29 17:40:26 -0700

    fix commands for rebuild

--- hosts/thebeast/specialisations/default.nix
+++ hosts/thebeast/specialisations/default.nix
@@ -3,8 +3,5 @@
     system.nixos.tags = ["dev"];
     imports = [./dev];
     gaming.enable = false;
-    environment.shellAliases = {
-      rebuild-dev = "sudo nixos-rebuild boot --flake . && sudo /nix/var/nix/profiles/system/specialisation/dev/bin/switch-to-configuration switch";
-    };
   };
 }
