6ea81942e066032f565fad5cd89d8374291f8e90
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-05-08 17:00:33 -0700

    set safe default undervolt for thebeast

--- hosts/thebeast/graphics.nix
+++ hosts/thebeast/graphics.nix
@@ -2,7 +2,16 @@
   hardware.amdgpu.initrd.enable = true;
   hardware.amdgpu.overdrive.enable = true;
 
-  services.lact.enable = true;
+  services.lact = {
+    enable = true;
+    settings = {
+      daemon.log_level = "info";
+      gpus."1002:7550-1DA2:E490-0000:03:00.0" = {
+        power_cap = 334.0;
+        voltage_offset = -80;
+      };
+    };
+  };
 
   services.xserver = {
     enable = true;
