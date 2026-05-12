bacfbf0605f9f68985dc79a9c836282b3b302645
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-05-12 15:39:52 -0700

    add 1password autostart in fedora

--- hosts/jasonbk-fedora-MZ0319NF/programs/1password.nix
+++ hosts/jasonbk-fedora-MZ0319NF/programs/1password.nix
@@ -1,4 +1,19 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs._1password.enable = true;
   programs._1password.sshAuthSock.enable = true;
+
+  xdg.configFile."autostart/1password.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=1Password
+    Exec=${lib.getExe config.programs._1password.package} --silent
+    Icon=1password
+    StartupWMClass=1Password
+    Terminal=false
+    X-GNOME-Autostart-enabled=true
+  '';
 }
