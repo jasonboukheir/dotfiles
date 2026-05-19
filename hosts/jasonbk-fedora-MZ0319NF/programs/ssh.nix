ab8ef693084de98204d937bef493b248c14ed72e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-05-19 09:48:01 -0700

    fix ssh on fedora

--- hosts/jasonbk-fedora-MZ0319NF/programs/ssh.nix
+++ hosts/jasonbk-fedora-MZ0319NF/programs/ssh.nix
@@ -1,3 +1,20 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.ssh.enable = true;
+
+  # Fedora's OpenSSH rejects ~/.ssh/config when it resolves into /nix/store
+  # (group-writable store dir trips safe_path). Materialize it as a real
+  # 0600 file owned by the user after home-manager links its tree.
+  # TODO: drop once https://github.com/nix-community/home-manager/issues/322 lands a fix.
+  home.activation.sshConfigCopy = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    src="${config.home.homeDirectory}/.ssh/config"
+    if [ -L "$src" ]; then
+      real=$(readlink -f "$src")
+      rm "$src"
+      install -m 600 "$real" "$src"
+    fi
+  '';
 }
