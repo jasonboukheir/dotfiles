85c3c5b7fb1489fdd69c278bf8742192376ca48c
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-05-24 19:24:01 -0700

    try to make the gpu more stable

--- hosts/thebeast/graphics.nix
+++ hosts/thebeast/graphics.nix
@@ -16,8 +16,21 @@
         admin_group = "wheel";
       };
       gpus."1002:7550-1DA2:E490-0000:03:00.0" = {
-        power_cap = 334.0;
-        voltage_offset = -25;
+        power_cap = 304.0;
+        voltage_offset = -10;
+        fan_control_enabled = true;
+        fan_control_settings = {
+          mode = "curve";
+          temperature_key = "edge";
+          interval_ms = 500;
+          curve = {
+            "40" = 0.30;
+            "55" = 0.50;
+            "65" = 0.70;
+            "75" = 0.90;
+            "85" = 1.00;
+          };
+        };
       };
     };
   };
