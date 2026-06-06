9dc8d8474ccaf9c75adbb0840f28c6bd6196d550
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-06-06 11:05:23 -0700

    revert mac app util follows

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -9,11 +9,13 @@
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
-    mac-app-util = {
-      url = "github:hraban/mac-app-util";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
-      inputs.cl-nix-lite.inputs.nixpkgs.follows = "nixpkgs-darwin";
-    };
+    # Don't override nixpkgs: upstream pins an older nixpkgs whose SBCL (2.4.10)
+    # is known to build its Lisp deps. Newer SBCL (26.05's 2.6.4) changed the
+    # macro-char array internals and crashes the pinned named-readtables with
+    # "Bug in readtable iterators or concurrent access?" while building
+    # fare-quasiquote. TODO: drop pin once cl-nix-lite bumps named-readtables
+    # past melisgl/named-readtables@6eea566 (github:hraban/cl-nix-lite/issues).
+    mac-app-util.url = "github:hraban/mac-app-util";
 
     home-manager-darwin = {
       url = "github:nix-community/home-manager/release-26.05";
