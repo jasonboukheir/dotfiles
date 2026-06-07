17fde9401faa6037160966b1ac30643435fa934d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-06-07 14:37:57 -0700

    use authkey in tailscale in litus

--- hosts/litus/services/tailscale.nix
+++ hosts/litus/services/tailscale.nix
@@ -4,5 +4,11 @@
     openFirewall = true;
     extraUpFlags = ["--reset"];
     extraDaemonFlags = ["--encrypt-state=false"];
+    authKeyFile = config.age.secrets."tailscale/authkey".path;
+    authKeyParameters = {
+      baseURL = "https://headscale.sunnycareboo.com";
+      preauthorized = true;
+    };
   };
+  age.secrets."tailscale/authkey".file = ../secrets/tailscale/authkey.age;
 }
