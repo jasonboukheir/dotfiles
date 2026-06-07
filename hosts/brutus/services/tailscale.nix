47e41c67ebaa0a126840e2016f639a2e1562f003
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-06-07 14:52:31 -0700

    fix authkeys in brutus

--- hosts/brutus/services/tailscale.nix
+++ hosts/brutus/services/tailscale.nix
@@ -2,11 +2,12 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
+    extraUpFlags = [
+      "--login-server=https://${config.homelab.services.headscale.domain}"
+      "--reset"
+    ];
     extraDaemonFlags = ["--encrypt-state=false"];
     authKeyFile = config.age.secrets."tailscale/authkey".path;
-    authKeyParameters = {
-      baseURL = "https://${config.homelab.services.headscale.domain}";
-    };
   };
   age.secrets."tailscale/authkey".file = ../secrets/tailscale/authkey.age;
 }
