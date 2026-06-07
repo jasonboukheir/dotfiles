5b879b6f218a54d86bf70a77ad0d71c1c49c8ff1
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2026-06-07 15:08:59 -0700

    fix litus tailscale setup

--- hosts/litus/services/tailscale.nix
+++ hosts/litus/services/tailscale.nix
@@ -2,13 +2,14 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    extraUpFlags = ["--reset"];
+    extraUpFlags = [
+      # TODO: Get this hardcoded url out to use homelab module
+      "--login-server=https://headscale.sunnycareboo.com"
+      "--reset"
+    ];
+    # TODO: this is to prevent issues with TPM chips
     extraDaemonFlags = ["--encrypt-state=false"];
     authKeyFile = config.age.secrets."tailscale/authkey".path;
-    authKeyParameters = {
-      baseURL = "https://headscale.sunnycareboo.com";
-      preauthorized = true;
-    };
   };
   age.secrets."tailscale/authkey".file = ../secrets/tailscale/authkey.age;
 }
